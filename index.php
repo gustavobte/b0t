8d9653eb77c8a6a81c19ad2b853711df83e4c795
Gustavo Silva <gustavobte@gmail.com>
2017-05-02 15:38:26 -0300

    first commit

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-define('BOT_TOKEN', '968855759917216');
-define('VERIFY_TOKEN', 'EAANxK09okKABAIkuNdcaT0y2owiNoWTYJUS3lpWsIB4DZCeKKqO2tPbU8JIjmzWty2hTkF4QLmhq93qXsDxKgIjr6BWo22teziYIpffjtVLgOOgGMib2lyD7oMZA8ZAhpg6EmpV53gZB5LVfQFqiKHn2QYM3pMpdUZCsrgy46HAZDZD');
+define('BOT_TOKEN', 'EAANxK09okKABAIkuNdcaT0y2owiNoWTYJUS3lpWsIB4DZCeKKqO2tPbU8JIjmzWty2hTkF4QLmhq93qXsDxKgIjr6BWo22teziYIpffjtVLgOOgGMib2lyD7oMZA8ZAhpg6EmpV53gZB5LVfQFqiKHn2QYM3pMpdUZCsrgy46HAZDZD');
+define('VERIFY_TOKEN', '968855759917216');
 $hub_verify_token = null;
 //-----VEFICA O WEBHOOK-----//
 if(isset($_REQUEST['hub_challenge'])) {
